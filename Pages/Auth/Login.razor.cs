4e56a0534636d83cf6c2416ccf64a1a698c3d5ee
moeyanhtun-coding <moeyan227970@gmail.com>
2025-05-26 21:54:42 +0630

    [Configure] Login API Call

--- Pages/Auth/Login.razor.cs
+++ Pages/Auth/Login.razor.cs
@@ -1,7 +1,34 @@
+using System.Net.Http.Json;
+using HotelManagementSystem_Web.Models;
 using Microsoft.AspNetCore.Components;
+using Newtonsoft.Json;
 
 namespace HotelManagementSystem_Web.Pages.Auth;
 
-public partial class Login : ComponentBase
+public partial class Login
 {
+    LoginRequestModel _model = new LoginRequestModel();
+
+    private async Task HandleLogin()
+    {
+        try
+        {
+            var res = await _httpClient.PostAsJsonAsync("api/User/Login", _model);
+            if (res.IsSuccessStatusCode)
+            {
+                var jsonStr = await res.Content.ReadAsStringAsync();
+                var respModel = JsonConvert.DeserializeObject<BaseResponseModel>(jsonStr);
+                if (respModel.respCode == "200")
+                {
+                    Console.WriteLine("Hee Hee Har Har");
+                    _navigation.NavigateTo("/user-home");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
 }

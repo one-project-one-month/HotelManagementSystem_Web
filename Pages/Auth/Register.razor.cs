d9d1f612d724bb660d473b91cbebd3532fe03ee2
moeyanhtun-coding <moeyan227970@gmail.com>
2025-05-26 21:56:03 +0630

    [Configure] Register API Call

--- Pages/Auth/Register.razor.cs
+++ Pages/Auth/Register.razor.cs
@@ -1,7 +1,37 @@
-using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+using HotelManagementSystem_Web.Models;
+using HotelManagementSystem_Web.Models.Register;
+using Newtonsoft.Json;
 
 namespace HotelManagementSystem_Web.Pages.Auth;
 
-public partial class Register : ComponentBase
+public partial class Register
 {
+    private RegisterRequestModel  _model = new RegisterRequestModel();
+
+    protected override async Task OnInitializedAsync()
+    {
+        
+    }
+    private async Task HandleRegister()
+    {
+        try
+        {
+            var res = await _httpClient.PostAsJsonAsync("api/User/Register", _model);
+            if (res.IsSuccessStatusCode)
+            {
+                var jsonStr = await res.Content.ReadAsStringAsync();
+                var resModel = JsonConvert.DeserializeObject<BaseResponseModel>(jsonStr);
+                if (resModel.respCode == "200")
+                {
+                    _navigation.NavigateTo("/login");
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
 }

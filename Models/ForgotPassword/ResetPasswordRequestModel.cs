5d076056550c8d9d1536c90b967564081609d963
Moe Yan Htun <moeyan227970@gmail.com>
2025-05-29 23:22:34 +0630

    -

---
+++ Models/ForgotPassword/ResetPasswordRequestModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HotelManagementSystem_Web.Models.ForgotPassword;
+
+public class ResetPasswordRequestModel
+{
+    public string Email { get; set; }
+    public string OTP { get; set; }
+    public string password { get; set; }
+}
---
+++ Pages/Auth/ResetPassword.razor.js
@@ -0,0 +1,5 @@
+﻿export class ResetPassword {
+  
+}
+
+window.ResetPassword = ResetPassword;

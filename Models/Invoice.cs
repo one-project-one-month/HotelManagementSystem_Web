2e5ad979c9476099174d58659d56704960e90109
Paing Soe Thu <95268659+paingsoethu-coding@users.noreply.github.com>
2025-06-06 01:28:06 +0700

    feat: add invoice codes

---
+++ Models/Invoice.cs
@@ -0,0 +1,20 @@
+﻿namespace HotelManagementSystem_Web.Models;
+
+public class Invoice
+{
+    public Guid InvoiceId { get; set; }
+    public string InvoiceCode { get; set; } = string.Empty;
+    public DateTime CheckInTime { get; set; }
+    public DateTime CheckOutTime { get; set; }
+    public decimal Deposite { get; set; }
+    public decimal? ExtraCharges { get; set; }
+    public decimal TotalAmount { get; set; }
+    public string? PaymentType { get; set; }
+    public GuestInfo? Guest { get; set; }
+}
+
+public class GuestInfo
+{
+    public string Nrc { get; set; } = string.Empty;
+    public string PhoneNo { get; set; } = string.Empty;
+}
---
+++ wwwroot/js/download.js
@@ -0,0 +1,7 @@
+﻿
+window.downloadFileFromBase64 = (base64, fileName, mimeType) => {
+    const link = document.createElement('a');
+    link.href = `data:${mimeType};base64,${base64}`;
+    link.download = fileName;
+    link.click();
+};

50d73aeac1af296433570aaa642440a5dacae143
Thet Ngon Phoo <thetngyonphoo@gmail.com>
2025-06-09 22:01:19 +0630

    Modify Admin User Profile

---
+++ Models/AdminUserInfoModel.cs
@@ -0,0 +1,14 @@
+﻿namespace HotelManagementSystem_Web.Models
+{
+    public class AdminUserInfoModel
+    {
+        public Guid UserId { get; set; }
+        public string UserName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string RoleName { get; set; } = null!;
+        public string? Gender { get; set; }
+        public string? Address { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
+        public DateTime? CreatedAt { get; set; }
+    }
+}
--- Models/UserProfile.cs
+++ Models/UserProfile.cs
@@ -2,16 +2,15 @@
 {   
     public class UserProfile
     {
-        public Guid UserId { get; set; }
+        public Guid? UserId { get; set; }
         public string? UserName { get; set; }
         public string? Email { get; set; }
-        public string? PhoneNumber { get; set; }
-        public Guid RoleId { get; set; }
-        public string? Password { get; set; }
-        public int Points { get; set; }
-        public string? ProfileImg { get; set; }
+        public string? PhoneNumber { get; set; }       
+        public string? Password { get; set; }      
+        public string? ProfileImg { get; set; }      
+        public string? ProfileImgMimeType { get; set; }
         public Gender Gender { get; set; }
-        public DateTime? DateOfBirth { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
         public string Address { get; set; }
     }
 

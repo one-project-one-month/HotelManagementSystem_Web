11b9c275f8c87b729d311d0de6d5858ffc00ccbf
Moe Yan Htun <moeyan227970@gmail.com>
2025-06-11 20:50:41 +0630

    [Refactor] Admin Home CS

--- Pages/Admin/AdminHome.razor.cs
+++ Pages/Admin/AdminHome.razor.cs
@@ -1,7 +1,33 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace HotelManagementSystem_Web.Pages.Admin;
 
-public partial class AdminUserProfile
+public partial class AdminHome : ComponentBase
 {
+    protected override async Task OnInitializedAsync()
+    {
+        await GetBookingList();
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await JSRuntime.InvokeVoidAsync("setColumnChartRoomType");
+            await JSRuntime.InvokeVoidAsync("setLineChartSale");
+            await JSRuntime.InvokeVoidAsync("setPieChartSourcesBooking");
+            await JSRuntime.InvokeVoidAsync("setBarChartCount");
+        }
+    }
+
+    public async Task GetBookingList()
+    {
+        var res = await _httpClient.GetAsync("Admin/Bookings");
+        if (res.IsSuccessStatusCode)
+        {
+            var jsonStr = await res.Content.ReadAsStringAsync();
+            Console.WriteLine(jsonStr);
+        }
+    }
 }
